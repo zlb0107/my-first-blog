36cf6bf72ad4dfe9d6760a92593218218606ab34
zhulubin <2911212504@qq.com>
2018-08-07 22:26:18 +0800

    Added views to create/edit blog post inside the site.

---
+++ blog/forms.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from django import forms
+
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
